f92a57e37f3b58a11b8f7339ee4b5e82b94101f4
Oleg <chukoleg@gmail.com>
2016-10-24 12:42:08 +0300

    pa2(read)(20161024)
    Fixed read dates.
    Added custon FileInfo class for creating files/directores

--- Console/dar/Archive reader.cs
+++ Console/dar/Archive reader.cs
@@ -14,6 +14,7 @@ namespace dar
             //System.IO.StreamReader StreamOfAr = new System.IO.StreamReader(ArchivePath);
             System.IO.BinaryReader BinFileReader = new System.IO.BinaryReader(StreamOfAr);
 
+            Environment.CurrentDirectory = DestinationPath;
             this.MakeFileFromArchive(DestinationPath, BinFileReader);
 
             BinFileReader.Close();
@@ -30,26 +31,50 @@ namespace dar
             }*/
             char buff;
             
+            
+            //var FileAttributes = (System.IO.FileAttributes)BinFileReader.ReadInt32();               ///Атрибуты
+
+            //Console.WriteLine(FileAttributes);
+
+            BufferedFileInfo FileInfo = new BufferedFileInfo();
+
             buff = BinFileReader.ReadChar();
             this.Method = BinFileReader.ReadInt16();                                                ///Method
+
             buff = BinFileReader.ReadChar();
-            var FileAttributes = (System.IO.FileAttributes)BinFileReader.ReadInt32();               ///Атрибуты
-            Console.WriteLine(FileAttributes);
+            FileInfo.FileAttributes = (System.IO.FileAttributes)BinFileReader.ReadInt32();
+
+            buff = BinFileReader.ReadChar();
+            FileInfo.FileCreationTime = new DateTime(BinFileReader.ReadInt64());
+            buff = BinFileReader.ReadChar();
+            FileInfo.FileLastAccessTime = new DateTime(BinFileReader.ReadInt64());
+            buff = BinFileReader.ReadChar();
+            FileInfo.FileLastWriteTime = new DateTime(BinFileReader.ReadInt64());
+
+            /*if ((FileAttributes & System.IO.FileAttributes.Directory) == System.IO.FileAttributes.Directory)
+            {
+                this.DirectoryCreator(Path, BinFileReader, FileAttributes);
+            }
+            else
+            {
+                this.FileCreator(Path, BinFileReader, FileAttributes);
+            }*/
+
             //Console.WriteLine((System.IO.FileAttributes)BinFileReader.ReadInt32());
 
-            System.IO.DirectoryInfo FileAttrib = new System.IO.DirectoryInfo(Path);
+            /*System.IO.DirectoryInfo FileAttrib = new System.IO.DirectoryInfo(Path);
 
             FileAttrib.Attributes = FileAttributes;
 
             FileAttrib.Create();
 
-            buff = BinFileReader.ReadChar();
-            FileAttrib.CreationTime = new DateTime(BinFileReader.ReadInt64());                      ///Время создания
-            buff = BinFileReader.ReadChar();
-            FileAttrib.LastAccessTime = new DateTime(BinFileReader.ReadInt64());                    ///Время последнего доступа
-            buff = BinFileReader.ReadChar();
-            FileAttrib.LastWriteTime = new DateTime(BinFileReader.ReadInt64());                     ///Время последней записи
-            buff = BinFileReader.ReadChar();
+            buff = BinFileReader.ReadChar();*/
+            //FileAttrib.CreationTime = new DateTime(BinFileReader.ReadInt64());                      ///Время создания
+            //buff = BinFileReader.ReadChar();
+            //FileAttrib.LastAccessTime = new DateTime(BinFileReader.ReadInt64());                    ///Время последнего доступа
+            //buff = BinFileReader.ReadChar();
+            //FileAttrib.LastWriteTime = new DateTime(BinFileReader.ReadInt64());                     ///Время последней записи
+            //buff = BinFileReader.ReadChar();
 
             /*if ((FileAttributes & System.IO.FileAttributes.Directory) == System.IO.FileAttributes.Directory)
             {
@@ -61,10 +86,10 @@ namespace dar
                 System.IO.File.Create(Path);
                 this.FileCreator(Path, BinFileReader);
             }*/
-            
+
 
             //BinFileReader.ReadString Считать имя
-            buff = BinFileReader.ReadChar();
+            //buff = BinFileReader.ReadChar();
             //buff = BinFileReader.ReadChar();
             //Считать путь
             //BinFileReader.(FileAttrib.DirectoryName);
@@ -72,9 +97,14 @@ namespace dar
             //buff = BinFileReader.ReadChar();
             //buff = BinFileReader.ReadChar();
         }
-        public void DirectoryCreator(System.IO.DirectoryInfo DirInfo, string Path, System.IO.BinaryReader BinFileReader)
+        public void DirectoryCreator(string Path, System.IO.BinaryReader BinFileReader, System.IO.FileAttributes Attribs)
         {
             char buff;
+
+            System.IO.DirectoryInfo DirInfo = new System.IO.DirectoryInfo(Path);
+
+            DirInfo.Attributes = Attribs;
+            
             buff = BinFileReader.ReadChar();
             DirInfo.CreationTime = new DateTime(BinFileReader.ReadInt64());                      ///Время создания
             buff = BinFileReader.ReadChar();
@@ -83,10 +113,13 @@ namespace dar
             DirInfo.LastWriteTime = new DateTime(BinFileReader.ReadInt64());                     ///Время последней записи
             buff = BinFileReader.ReadChar();
         }
-        public void FileCreator(string Path, System.IO.BinaryReader BinFileReader)
+        public void FileCreator(string Path, System.IO.BinaryReader BinFileReader, System.IO.FileAttributes Attribs)
         {
             char buff;
             System.IO.FileInfo CurFileInfo = new System.IO.FileInfo(Path);
+
+            CurFileInfo.Attributes = Attribs;
+
             buff = BinFileReader.ReadChar();
             CurFileInfo.CreationTime = new DateTime(BinFileReader.ReadInt64());                      ///Время создания
             buff = BinFileReader.ReadChar();
