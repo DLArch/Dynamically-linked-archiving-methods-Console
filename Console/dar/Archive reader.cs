4261f5405acb2c9b2ef949f0f8cef69bd1065225
Oleg <chukoleg@gmail.com>
2016-11-07 12:22:14 +0300

    TempCommitFM(20161107)

---
+++ Console/dar/Archive reader.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dar
+{
+    public class Archive_reader
+    {
+        /// <summary>
+        /// Распаковывает архив из ArchivePath в папку DestinationPath,
+        /// создавая ее если она отсутствует.
+        /// </summary>
+        /// <param name="ArchivePath"> Абсолютный/относительный путь к архиву </param>
+        /// <param name="DestinationPath"> Абсолютный/относительный путь к папке </param>
+        public Archive_reader(string ArchivePath, string DestinationPath)
+        {
+            if (!System.IO.Directory.Exists(System.Environment.CurrentDirectory + System.IO.Path.GetDirectoryName(Archive_reader.LogFilePath)))
+            {
+                System.IO.Directory.CreateDirectory(System.Environment.CurrentDirectory + System.IO.Path.GetDirectoryName(Archive_reader.LogFilePath));
+            }
+
+            System.IO.FileStream StreamOfLog = new System.IO.FileStream((Environment.CurrentDirectory + Archive_reader.LogFilePath), System.IO.FileMode.Append, System.IO.FileAccess.Write);
+            System.IO.BinaryWriter BinLogWriter = new System.IO.BinaryWriter(StreamOfLog);
+
+            System.IO.FileStream StreamOfAr = new System.IO.FileStream(ArchivePath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+            System.IO.BinaryReader BinFileReader = new System.IO.BinaryReader(StreamOfAr);
+
+            if (System.IO.Path.IsPathRooted(DestinationPath))
+            {
+                System.Environment.CurrentDirectory = DestinationPath;
+            }
+            else
+            {
+                System.Environment.CurrentDirectory += System.IO.Path.DirectorySeparatorChar + DestinationPath;
+            }
+
+            BufferedFileInfo FileInfo = new BufferedFileInfo();
+
+            FileInfo.LogFileHandle = BinLogWriter;
+
+            this.MakeFileFromArchive("", BinFileReader, FileInfo);
+
+            BinFileReader.Close();
+            StreamOfAr.Close();
+
+            BinLogWriter.Close();
+            StreamOfLog.Close();
+        }
+        public void MakeFileFromArchive(string Path, System.IO.BinaryReader BinFileReader, BufferedFileInfo FileInfo)
+        {
+            FileInfo.IsFolder = false;
+            FileInfo.NotReadFile = false;
+
+            ++BinFileReader.BaseStream.Position;                                                                        ///|
+            this.Method = BinFileReader.ReadInt16();                                                                    ///|Method
+            ++BinFileReader.BaseStream.Position;                                                                        ///|Method|
+            FileInfo.FileAttributes = (System.IO.FileAttributes)BinFileReader.ReadInt32();                              ///|Method|Атрибуты
+            ++BinFileReader.BaseStream.Position;                                                                        ///|Method|Атрибуты|
+            FileInfo.FileCreationTime = new DateTime(BinFileReader.ReadInt64());                                        ///|Method|Атрибуты|Д
+            ++BinFileReader.BaseStream.Position;                                                                        ///|Method|Атрибуты|Д|
+            FileInfo.FileLastAccessTime = new DateTime(BinFileReader.ReadInt64());                                      ///|Method|Атрибуты|Д|Д
+            ++BinFileReader.BaseStream.Position;                                                                        ///|Method|Атрибуты|Д|Д|
+            FileInfo.FileLastWriteTime = new DateTime(BinFileReader.ReadInt64());                                       ///|Method|Атрибуты|Д|Д|Д
+            ++BinFileReader.BaseStream.Position;                                                                        ///|Method|Атрибуты|Д|Д|Д|
+
+            ++BinFileReader.BaseStream.Position;
+            FileInfo.FileName = "";
+            for (this.buff = BinFileReader.ReadChar(); this.buff != Archive_reader.FileNameDelim; this.buff = BinFileReader.ReadChar())
+            {
+                FileInfo.FileName += this.buff;
+            }                                                                                                           ///|Method|Атрибуты|Д|Д|Д|-Name|
+
+            ++BinFileReader.BaseStream.Position;
+            FileInfo.FileDirectoryName = "";
+            for (this.buff = BinFileReader.ReadChar(); this.buff != Archive_reader.FileNameDelim; this.buff = BinFileReader.ReadChar())
+            {
+                FileInfo.FileDirectoryName += this.buff;
+            }                                                                                                           ///|Method|Атрибуты|Д|Д|Д|-Name|-Path|
+
+            if ((FileInfo.FileAttributes & System.IO.FileAttributes.Directory) == System.IO.FileAttributes.Directory)
+            {
+                FileInfo.IsFolder = true;                                                                               ///|Method|Атрибуты|Д|Д|Д|-Name|-Path|
+            }
+            else
+            {
+                FileInfo.FileLength = BinFileReader.ReadInt64();                                                        ///|Method|Атрибуты|Д|Д|Д|-Name|-Path|12345
+            }
+            ++BinFileReader.BaseStream.Position;                                                                        ///|Method|Атрибуты|Д|Д|Д|-Name|-Path|12345|
+
+            /// Создание файла/папки
+            FileInfo.MakeFile(Path);
+
+            /// Запись в файл
+            if (!FileInfo.IsFolder)
+            {
+                try
+                {
+                    /// Изменение указателя в потоке при невозможности записи в файл
+                    if (FileInfo.NotReadFile)
+                    {
+                        BinFileReader.BaseStream.Position += FileInfo.FileLength;
+                    }
+                    else
+                    {
+                        /// Проверка на повреждение последнего файла в архиве
+                        if ((BinFileReader.BaseStream.Position + FileInfo.FileLength) <= BinFileReader.BaseStream.Length)
+                        {
+                            using (System.IO.FileStream bFS = new System.IO.FileStream(System.Environment.CurrentDirectory + FileInfo.PathModifier(Path), System.IO.FileMode.Open, System.IO.FileAccess.Write))
+                            {
+                                for (FileInfo.PosBuff = 0; FileInfo.PosBuff < FileInfo.FileLength; ++FileInfo.PosBuff)
+                                {
+                                    this.ByteBuff = BinFileReader.ReadByte();
+                                    bFS.WriteByte(this.ByteBuff);
+                                }
+                                bFS.Close();
+                            }
+                        }
+                        else
+                        {
+                            FileInfo.LogFileHandle.Write("Файл " + System.Environment.CurrentDirectory + FileInfo.PathModifier(Path) + " поврежден при записи и не был разархивирован!");
+                            Console.WriteLine("Файл {0} поврежден при записи и не был разархивирован!", System.Environment.CurrentDirectory + FileInfo.PathModifier(Path));
+                            return;
+                        }
+                    }
+                }
+                catch
+                {
+                    FileInfo.LogFileHandle.Write("Программа не может получить доступ к " + System.Environment.CurrentDirectory + FileInfo.PathModifier(Path));
+                    Console.WriteLine("Программа не может получить доступ к {0}", System.Environment.CurrentDirectory + FileInfo.PathModifier(Path));
+                    BinFileReader.BaseStream.Position += FileInfo.FileLength;
+                }
+            }
+
+            /// Запись атрибутов
+            if (!FileInfo.NotReadFile)
+            {
+                FileInfo.WriteAttribs(System.Environment.CurrentDirectory + FileInfo.PathModifier(Path));
+            }
+
+            if (BinFileReader.BaseStream.Position < BinFileReader.BaseStream.Length)
+            {
+                MakeFileFromArchive(Path, BinFileReader, FileInfo);
+            }
+        }
+        /// <summary>
+        /// Символьный буфер
+        /// </summary>
+        public char buff
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// Байтовый буфер. Используется
+        /// для снятия данных из архива
+        /// </summary>
+        public byte ByteBuff
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// Номер метода обрабатываемого файла
+        /// </summary>
+        public Int16 Method
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// Разделитель в файловой записи архива
+        /// </summary>
+        public const char FileNameDelim = '|';
+        public const string LogFilePath = @"\Logs\Log.dlal";
+    }
+}
